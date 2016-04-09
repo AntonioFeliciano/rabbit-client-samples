da0de56169bcfb26526917d40e7d4bee89e9dd9f
Neil Sorensen <nsorens@gmail.com>
2016-04-09 07:17:11 -0600

    Add a starter to CSharp to make it simpler for attendees

---
+++ c-sharp/CodeCamp.Starter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using RabbitClient;
+
+namespace CodeCamp.Starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new RabbitConnectionSettings
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                Port = 5672,
+                VirtualHost = "/"
+            };
+            var client = new SimpleRabbitClient(settings, new JsonByteArraySerializer());
+
+        }
+    }
+
+    public class OrderFilledMessage
+    {
+        public string Customer { get; set; }
+        public string Address { get; set; }
+        public List<Pizza> Pizzas { get; set; }
+        public string CouponCode { get; set; }
+        public decimal OrderPrice { get; set; }
+    }
+
+    public class Pizza
+    {
+        public string Size { get; set; }
+        public List<string> Toppings { get; set; }
+    }
+
+    public class MyOrder
+    {
+        public string Customer { get; set; }
+        public string Address { get; set; }
+        public List<Pizza> Pizzas { get; set; }
+    }
+
+    public class CouponOrder : MyOrder
+    {
+        public string Coupon { get; set; }
+    }
+}
